f23c5a39100440161641d10dfab63b062990d277
Jun Sugahara <j.sghr.jp@gmail.com>
2012-06-06 09:37:00 +0900

    delta_t to num

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ class Main
   db_user_name = ARGV[2]
   db_pass = ARGV[3]
   db_name = ARGV[4]
-  delta_t = ARGV[5]
+  delta_t = ARGV[5].to_f
 
   db = Mysql::new(db_host, db_user_name, db_pass, db_name)
   
