59612af87abf5469b005fd7ed325372d495ebbe3
Jun Sugahara <j.sghr.jp@gmail.com>
2012-06-11 16:02:28 +0900

    add packet analyzer & file output

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 class Main
   require './analyzer.rb'
   require 'mysql'
+  require './io.rb'
   table_name = ARGV[0]
   db_host = ARGV[1]
   db_user_name = ARGV[2]
@@ -9,13 +10,22 @@ class Main
   delta_t = ARGV[5].to_f
 
   db = Mysql::new(db_host, db_user_name, db_pass, db_name)  
-  analyzer = Analyzer.new(table_name, db, delta_t)
-  puts "get_tcp_flow"
-  analyzer.get_tcp_flow
-  puts "get_udp_flow"
-  analyzer.get_udp_flow
-  puts "get_all_flow"
-  analyzer.get_all_flow
-  
+  tables = db.list_tables
+  tables.each do |tbl|
+    puts tbl
+    analyzer = Analyzer.new(tbl, db, delta_t)
+    puts "get_tcp_flow"
+    IO.to_file(analyzer.get_tcp_flow, "tcp_flow"+delta_t+"_"+tbl+".log")
+    puts "get_udp_flow"
+    IO.to_file(analyzer.get_udp_flow, "udp_flow"+delta_t+"_"+tbl+".log")
+    puts "get_all_flow"
+    IO.to_file(analyzer.get_all_flow, "all_flow"+delta_t+"_"+tbl+".log")
+    puts "get_tcp_packet"
+    IO.to_file(analyzer.get_tcp_packet, "tcp_packet"+delta_t+"_"+tbl+".log")
+    puts "get_udp_packet"
+    IO.to_file(analyzer.get_udp_packet, "udp_packet"+delta_t+"_"+tbl+".log")
+    puts "get_all_packet"
+    IO.to_file(analyzer.get_all_packet, "all_packet"+delta_t+"_"+tbl+".log")
+  end
 
 end
