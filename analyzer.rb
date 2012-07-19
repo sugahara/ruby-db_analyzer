c3e1f299a2cd2a73b6a5f6439ee17c2b84f22279
Jun Sugahara <j.sghr.jp@gmail.com>
2012-07-19 10:10:28 +0900

    add protocol layer option and serious bug fix

--- analyzer.rb
+++ analyzer.rb
@@ -39,25 +39,24 @@ class Analyzer
     @end_time = @start_time + @delta_t -1
   end
 
-  def get_flow_sql(table, start_time, end_time, protocol=nil)
+  def get_flow_sql(table, start_time, end_time, protocol=nil, layer_num = nil)
     sql = "SELECT COUNT(DISTINCT ip_src, ip_dst, port_src, port_dst) FROM `#{table}` WHERE time BETWEEN '#{start_time.strftime("%Y-%m-%d %H:%M:%S")}' AND '#{end_time.strftime("%Y-%m-%d %H:%M:%S")}'"
     if protocol != nil
-      sql +=  "AND protocol_3='#{protocol}'"
+      sql +=  "AND protocol_#{layer_num}='#{protocol}'"
     end
-    sql += "AND time  BETWEEN '2012-05-26 00:00:00' AND '2012-06-01 23:59:59'"
     sql
   end
 
-  def get_packet_sql(table, start_time, end_time, protocol=nil)
+  def get_packet_sql(table, start_time, end_time, protocol=nil, layer_num = nil)
     sql = "SELECT COUNT(*) FROM `#{table}` WHERE time BETWEEN '#{start_time.strftime("%Y-%m-%d %H:%M:%S")}' AND '#{end_time.strftime("%Y-%m-%d %H:%M:%S")}'"
     if protocol != nil
-      sql +=  "AND protocol_3='#{protocol}'"
+      sql +=  "AND protocol_#{layer_num}='#{protocol}'"
     end
-    sql += "AND time  BETWEEN '2012-05-26 00:00:00' AND '2012-06-01 23:59:59'"
     sql
   end
 
-  def flow_query(protocol=nil)
+  def flow_query(protocol=nil, layer_num = nil)
+    # プロトコルが指定されている場合のフロー数集計
     if protocol != nil
       @tables.each_with_index do |tbl, i|
         @processing_table = tbl
@@ -65,14 +64,14 @@ class Analyzer
         while @start_time <= @last_packet_time
           p @start_time
           p @end_time
-          sql = get_flow_sql(@processing_table, @start_time, @end_time, protocol)
+          sql = get_flow_sql(@processing_table, @start_time, @end_time, protocol, layer_num)
           result = @db.query(sql).fetch_row()[0].to_i
           ## table transition
           j = i
           while @end_time >= @last_packet_time && @tables.size - 1 < j
             @processing_table = @tables[j+1]
             time_check
-            sql = get_packet_sql(@tables[j+1], @start_time, @end_time, protocol)
+            sql = get_flow_sql(@tables[j+1], @start_time, @end_time, protocol, layer_num)
             result += @db.query(sql).fetch_row()[0].to_i
             j += 1
           end
@@ -85,6 +84,7 @@ class Analyzer
         IO::to_file(@result,"#{protocol}_flow"+@delta_t.to_s+"_"+@processing_table+".log")
         @result = []
       end
+      # プロトコルが指定されていない場合のフロー数集計
     else
       @tables.each_with_index do |tbl, i|
         @processing_table = tbl
@@ -97,7 +97,7 @@ class Analyzer
           while @end_time >= @last_packet_time && @tables.size - 1 < j
             @processing_table = @tables[j+1]
             time_check
-            sql = get_packet_sql(@tables[j+1], @start_time, @end_time)
+            sql = get_flow_sql(@tables[j+1], @start_time, @end_time)
             result += @db.query(sql).fetch_row()[0].to_i
             j += 1
           end
@@ -113,7 +113,8 @@ class Analyzer
     end
   end
 
-  def packet_query(protocol=nil)
+  def packet_query(protocol=nil, layer = nil)
+    # プロトコルが指定されている場合のパケット数集計
     if protocol != nil
       @tables.each_with_index do |tbl, i|
         @processing_table = tbl
@@ -121,14 +122,14 @@ class Analyzer
         while @start_time <= @last_packet_time
           p @start_time
           p @end_time
-          sql = get_packet_sql(@processing_table, @start_time, @end_time, protocol)
+          sql = get_packet_sql(@processing_table, @start_time, @end_time, protocol, layer)
           result = @db.query(sql).fetch_row()[0].to_i
           ## table transition
           j = i
           while @end_time >= @last_packet_time && @tables.size - 1 < j
             @processing_table = @tables[j+1]
             time_check
-            sql = get_packet_sql(@tables[j+1], @start_time, @end_time, protocol)
+            sql = get_packet_sql(@tables[j+1], @start_time, @end_time, protocol, layer)
             result += @db.query(sql).fetch_row()[0].to_i
             j += 1
           end
@@ -141,6 +142,7 @@ class Analyzer
         IO::to_file(@result,"#{protocol}_packet"+@delta_t.to_s+"_"+@processing_table+".log")
         @result = []
       end
+      # プロトコルが指定されていない場合のパケット数集計
     else
       @tables.each_with_index do |tbl, i|
         @processing_table = tbl
@@ -196,12 +198,12 @@ class Analyzer
     packet_query()
   end
 
-  def get_packet(protocol)
-    packet_query(protocol)
+  def get_packet(protocol, layer)
+    packet_query(protocol, layer)
   end
 
-  def get_flow(protocol)
-    flow_query(protocol)
+  def get_flow(protocol, layer)
+    flow_query(protocol, layer)
   end
 
 end
